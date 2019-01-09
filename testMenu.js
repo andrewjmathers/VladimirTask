54f9f5107b2823b1497dbd79641247fe4c1418cf
Andrey Denisov <AndrewMathers@Andreys-MacBook-Air.local>
2019-01-09 02:35:01 +0100

    test ready

--- testMenu.js
+++ testMenu.js
@@ -1,31 +1,48 @@
 const 
     testMenuWrapper = document.getElementById('testMenuWrapper'),
     loading = document.getElementById('loading'),
-    apiUrl = 'https://api.myjson.com/bins/11s7v4',
+    testBoxClass = document.getElementsByClassName("testBox"),
+    apiUrl = 'https://api.myjson.com/bins/fkzhk',
 
     loadTests = () =>{
+        testMenuWrapper.innerHTML = "";
         loading.classList.remove('invisible');
         fetch(apiUrl)
             .then((resp)=>resp.text())
             .then((resp)=>JSON.parse(resp))
             .then((response)=>{
-                response.map((entry)=>{
+                response.map((entry, i)=>{
                     let 
                         testDivBox = document.createElement("div"),
                         testDivLabel = document.createElement("h3"),
-                        testDivResult = document.createElement("h5");
+                        testDivResult = document.createElement("h4");
 
                     testDivLabel.innerHTML = entry.name;
-                    testDivResult.innerHTML = `Score: ${localStorage[entry.name] || 0} / ${entry.test.length}`;
+                    testDivResult.innerHTML = `Score: ${localStorage[entry.name] || "not started"}`;
                     
+                    testDivBox.setAttribute("key", i);
+
                     testDivBox.classList.add("testBox");
                     testDivResult.classList.add("resultScore");
 
-                    testDivLabel.appendChild(testDivResult);
+                    testDivBox.appendChild(testDivResult);
                     testDivBox.appendChild(testDivLabel);
                     testMenuWrapper.appendChild(testDivBox);
                 })
-            loading.classList.add('invisible');
-            testMenuWrapper.classList.remove('invisible');
+                loading.classList.add('invisible');
+                testMenuWrapper.classList.remove('invisible');
+                
+                [].forEach.call(testBoxClass, (testBox)=>{
+                    testBox.onclick = (e) =>{
+                        const target = e.target.className != "testBox"?
+                            e.target.parentNode
+                        :
+                            e.target
+                        ;
+                        testMenuWrapper.classList.add("invisible");
+                        testScreen(response[target.attributes.key.nodeValue]);
+                    };
+                });
             });
     };
+
